3f3fb2fd95d4d2ab83bac6cf408083a2080953fd
Jodaille <jody.noury@gmail.com>
2016-01-22 09:34:15 +0100

    ajout exemple reglage horloge TinyRTC

---
+++ TinyRtcPetiteHorloge/TinyRtcPetiteHorloge.ino
@@ -0,0 +1,45 @@
+/**
+ * Tiny RTC module
+ * module horloge
+ * 
+ * DS1307 pour bus I2C
+ * avec batterie au lithium CR1225
+ * 
+ * Le port I2C de l'Arduino est situé
+ * sur les pin A4 et A5
+ * 
+ * Analog pin A5 <-> SCL
+ * Analog pin A4 <-> SDA
+ */
+
+#include <Wire.h>
+#include "RTClib.h"
+RTC_DS1307 RTC;
+
+void setup () {
+    Serial.begin(9600);
+    Wire.begin();
+    RTC.begin();
+  if (! RTC.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // la ligne suivante permet d'ajuster la date & l'heure
+    // à la date de compilation du sketc
+    RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
+}
+void loop () {
+    DateTime now = RTC.now(); 
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println(); 
+    delay(1000);
+}
