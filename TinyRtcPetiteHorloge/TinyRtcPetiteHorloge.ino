381c9f5c2978fc66531ad375624981bdfe3471fd
Jodaille <jody.noury@gmail.com>
2016-01-22 19:38:06 +0100

    correction definition heure + commentaire lib RTClib

--- TinyRtcPetiteHorloge/TinyRtcPetiteHorloge.ino
+++ TinyRtcPetiteHorloge/TinyRtcPetiteHorloge.ino
@@ -1,20 +1,29 @@
 /**
  * Tiny RTC module
  * module horloge
- * 
+ *
  * DS1307 pour bus I2C
  * avec batterie au lithium CR1225
- * 
+ *
  * Le port I2C de l'Arduino est situé
  * sur les pin A4 et A5
- * 
+ *
  * Analog pin A5 <-> SCL
  * Analog pin A4 <-> SDA
  */
 
 #include <Wire.h>
+/**
+ * Nous utilisons la librairie RTClib
+ * cf: https://github.com/adafruit/RTClib/archive/master.zip
+ * à installer dans le répertoire libraries
+ * du répertoire de sketchs
+ */
 #include "RTClib.h"
 RTC_DS1307 RTC;
+
+// pour ajuster l'heure changer en : true
+bool definirHeure = false;
 
 void setup () {
     Serial.begin(9600);
@@ -22,13 +31,18 @@ void setup () {
     RTC.begin();
   if (! RTC.isrunning()) {
     Serial.println("RTC ne fonctionne pas!");
-    // la ligne suivante permet d'ajuster la date & l'heure
-    // à la date de compilation du sketc
-    RTC.adjust(DateTime(__DATE__, __TIME__));
   }
+
+  if(definirHeure)
+  {
+      // la ligne suivante permet d'ajuster la date & l'heure
+      // à la date de compilation du sketc
+      RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
+
 }
 void loop () {
-    DateTime now = RTC.now(); 
+    DateTime now = RTC.now();
     Serial.print(now.year(), DEC);
     Serial.print('/');
     Serial.print(now.month(), DEC);
@@ -40,6 +54,6 @@ void loop () {
     Serial.print(now.minute(), DEC);
     Serial.print(':');
     Serial.print(now.second(), DEC);
-    Serial.println(); 
+    Serial.println();
     delay(1000);
 }
