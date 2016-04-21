7b3bc78440b8c5cede6c519c633727f6531650c3
Jodaille <jody.noury@gmail.com>
2016-04-21 19:34:09 +0200

    repace ; by & to prepare GET query

--- ArduinoToLinux/ReadArduinoSerial.sh
+++ ArduinoToLinux/ReadArduinoSerial.sh
@@ -1,20 +1,23 @@
-#!/bin/sh
-
-searchstring=";"
-replacestring="#"
-while read -r line; do
-    echo ${line//$searchstring/$replacestring}
-    #${var//;/\&}
-done < /dev/stdin
-
-query="/a.php?$var"
-# http://stackoverflow.com/questions/918886/how-do-i-split-a-string-on-a-delimiter-in-bash#answer-15988793
+#!/home/jody/LyceeDesAndaines/ash
+# ash is a symlink to /bin/busybox
+# in order to "recreate" OpenWRT environment
+# Notes:
+# to know which shell is in use:
+# readlink -f $(which sh)
+#
+# script is launched as follow:
+# /home/jody/LyceeDesAndaines/ArduinoToLinux/ReadArduinoSerial.sh < /dev/ttyUSB2
 
 
-#echo "You entered '$query'"
-printf "GET $query HTTP/1.0\r\n\r\n"
+searchstring=';'
+replacestring='&'
 
-
+while read line; do
+    if [ ! -z "$line" ]; then # we do not want empty line
+        query=/a.php?${line//$searchstring/$replacestring} # test HTTP GET query
+        printf "GET $query HTTP/1.0\r\n\r\n"
+    fi
+done
 
 # https://forum.openwrt.org/viewtopic.php?pid=223959#p223959
 # printf "GET /a.php?id=nano&t=12&h=22&l=123 HTTP/1.0\r\n\r\n" |nc  jodaille.org 80
