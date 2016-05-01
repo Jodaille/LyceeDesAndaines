f14e9a040132955562faa88587b401840cce0d67
Jodaille <jody.noury@gmail.com>
2016-05-01 12:36:29 +0200

    ajout sketch lecture cpateur par son adresse

---
+++ LectureDS18B20ParSonAdresse/LectureDS18B20ParSonAdresse.ino
@@ -0,0 +1,71 @@
+/**
+ * Lecture de température avec un seul
+ * capteur DS18B20 (le premier)
+ */
+
+/** 
+ *  Nous utilisons la librairie OneWire
+ *  Elle doit être présente dans le répertoire libraries
+ *  situé dans le répertoire des croquis/sketchs
+ *  voir dans le menu Préférences
+ *  cf: https://github.com/PaulStoffregen/OneWire/archive/master.zip
+ */
+#include <OneWire.h>
+
+/**
+ * Nous utilisons aussi la librairie DallasTemperature
+ * cf: https://github.com/milesburton/Arduino-Temperature-Control-Library
+ */
+#include <DallasTemperature.h>
+ 
+// Le fil des données (jaune?) est relié au pin 10 du Arduino
+#define ONE_WIRE_BUS 10
+ 
+/** 
+ * Création d'une instance oneWire afin de pouvoir 
+ * communiquer avec des périphériques OneWire
+ * Nb: pas seulement des capteurs Maxim/Dallas
+ */ 
+OneWire oneWire(ONE_WIRE_BUS);
+ 
+/**
+ * Nous passons notre instance oneWire à notre 
+ * objet sensors utilisant DallasTemperature
+ */
+DallasTemperature sensors(&oneWire);
+
+/**
+ * Adresses du capteur, cf: https://git.io/vwM2x
+ * pour scanner l'adresse de votre capteur
+ */
+DeviceAddress temperature1 = { 0x28, 0x01, 0x37, 0x29, 0x07, 0x00, 0x00, 0x65 };
+
+void setup(void)
+{
+  // Initialisation du port série
+  Serial.begin(9600);
+  Serial.println("Demo de la librairie Dallas Temperature");
+
+  // Initialisation de notre librairie
+  sensors.begin();  
+}
+
+
+void loop(void)
+{
+  /**
+   * Nous interrogeons notre capteur
+   */
+  sensors.requestTemperatures(); // envoi de la demande
+
+
+  Serial.print("La temperature du premier capteur est : ");
+  /**
+   * temperature1 est un tableau contenant l'adresse
+   * de notre capteur.
+   * cf: https://git.io/vwM2x pour scanner les adresses
+   */
+  Serial.println(sensors.getTempC(temperature1));
+
+
+}
