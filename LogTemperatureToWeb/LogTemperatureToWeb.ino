5ce334796c8b9d1233d341abedc11c917bf1d664
Jodaille <jody.noury@gmail.com>
2016-05-06 23:58:52 +0200

    add RTC string to log

--- LogTemperatureToWeb/LogTemperatureToWeb.ino
+++ LogTemperatureToWeb/LogTemperatureToWeb.ino
@@ -82,6 +82,34 @@ const int chipSelect = 4;
 
 bool sDisReady = false;
 
+/**
+ * Tiny RTC module
+ * module horloge
+ *
+ * DS1307 pour bus I2C
+ * avec batterie au lithium CR1225
+ *
+ * Le port I2C de l'Arduino est situé
+ * sur les pin A4 et A5
+ *
+ * Analog pin A5 <-> SCL
+ * Analog pin A4 <-> SDA
+ */
+
+#include <Wire.h>
+/**
+ * Nous utilisons la librairie RTClib
+ * cf: https://github.com/adafruit/RTClib/archive/master.zip
+ * à installer dans le répertoire libraries
+ * du répertoire de sketchs
+ */
+#include "RTClib.h"
+RTC_DS1307 RTC;
+
+// pour ajuster l'heure changer en : true
+bool definirHeure = false;
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -96,6 +124,15 @@ void setup() {
   // Initialisation de notre librairie
   sensors.begin();  
 
+  Wire.begin();
+  RTC.begin();
+
+  if(definirHeure)
+  {
+      // la ligne suivante permet d'ajuster la date & l'heure
+      // à la date de compilation du sketc
+      RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
 }
 
 void loop() {
@@ -134,6 +171,10 @@ String builString()
 {
   String dataString = "id=rucheA;";
 
+  dataString += "time=";
+  dataString += buildTime();
+  dataString += ";";
+
   sensors.requestTemperatures(); // envoi de la demande
    /**
    * temperature1 est un tableau contenant l'adresse
@@ -164,6 +205,57 @@ String builString()
   dataString += String(millis());
   dataString += ";";
   return dataString;
+}
+
+/**
+ * build a string with time
+ */
+String buildTime()
+{
+  DateTime now = RTC.now();
+  String dateString = String(now.year());
+
+  dateString += "-";
+  int month = now.month();
+  if(month<10)
+  {
+    dateString += "0";
+  }
+  dateString += month;
+
+  dateString += "-";
+  int day = now.day();
+  if(day<10)
+  {
+    dateString += "0";
+  }
+  dateString += day;
+
+  dateString += " ";
+  int hour = now.hour();
+  if(hour<10)
+  {
+    dateString += "0";
+  }
+  dateString += hour;
+
+  dateString += ":";
+  int minute = now.minute();
+  if(minute<10)
+  {
+    dateString += "0";
+  }
+  dateString += minute;
+
+  dateString += ":";
+  int second = now.second();
+  if(second<10)
+  {
+    dateString += "0";
+  }
+  dateString += second;
+ 
+  return dateString;
 }
 
 float pression()
