dfcd4b3746e7eb91273decafa2f77340ecdab966
Jodaille <jody.noury@gmail.com>
2016-11-06 03:48:09 +0100

    add meteo stat. on click + style

--- snapshots/preview.php
+++ snapshots/preview.php
@@ -64,22 +64,153 @@ $jsonData = json_encode($aDatas);
     <head>
         <meta charset="utf-8">
         <title>Preview of beehive entry snapshots</title>
-    </head>
-<body>
-    <div class="preview">
-        <p>Daily images ( <?php echo $iNbImages; ?> ): <?php echo $ymd; ?>
-            <a href="/snapshots/preview.php?day=yesterday">yesterday</a>
-        </p>
+        <style>
+        body {
+            background-color:grey;
+        }
+        div[name="meteo"] {
+            display: none;
+        }
+        iframe {
+            width:300px;
+            height:200px;
+            border-image-repeat:stretch;
+        }
+        .informations{
+            position: absolute;
+            margin: 0px;
+            top:    0px;
+            left:   0px;
+            width: 800px;
+            height: 110px;
+            background-color:white;
+        }
 
-<?php
-echo '<img id="imgPreview" src="' . $firstImage['src'] . '" />'
-. '<span id="time">' . $firstImage['time'] . "</span>\n"
-. '<span id="selectedImage"><a id="currentImgLink" target="_blank">' . "</a></span>\n"
-;
-?>
+        .preview{
+            position: absolute;
+            margin: 0px;
+            top:    110px;
+            left:   0px;
+            width: 800px;
+            height: 600px;
+        }
+        .temperatures{
+            position: absolute;
+            margin: 0px;
+            top: 0px;
+            left: 790px;
+            width: 300px;
+            height: 200px;
+        }
+        .luminosity{
+            position: absolute;
+            margin: 0px;
+            top:  200px;
+            left:   790px;
+            width: 300px;
+            height: 200px;
+        }
+        .humidity{
+            position: absolute;
+            margin: 0px;
+            top:  400px;
+            left:   790px;
+            width: 300px;
+            height: 200px;
+        }
+        .atmospheric{
+            position: absolute;
+            margin: 0px;
+            top:    0px;
+            left:   1090px;
+            width: 300px;
+            height: 200px;
+        }
+        .rainfall{
+            position: absolute;
+            margin: 0px;
+            top:  200px;
+            left:   1090px;
+            width: 300px;
+            height: 200px;
+        }
+        .rainlevel{
+            position: absolute;
+            margin: 0px;
+            top:    400px;
+            left:   1090px;
+            width: 300px;
+            height: 200px;
+        }
+        .gitcode{
+            position: absolute;
+            margin: 0px;
+            top:    555px;
+            left:   0px;
+            width: 800px;
+            height: 45px;
+            /*background-color:white;*/
+        }
+        </style>
+    </head>
+<body style="">
+
+    <div class="informations">
+        <h1>Danvou Warré beehive</h1>
+
+        <p>Daily images ( <?php echo $iNbImages; ?> ): <?php echo $ymd; ?>
+            <span id="time"></span>
+            <span id="selectedImage"><a id="currentImgLink" target="_blank"></span>
+            <a href="/snapshots/preview.php?day=yesterday">(see yesterday)</a>
+            <a id="displayMeteo" href="#">display Meteo</a>
+        </p>
     </div>
-    <div name="gitcode">
-        <p>see: https://github.com/Jodaille/LyceeDesAndaines/blob/master/snapshots/preview.php</p>
+
+    <div class="preview">
+        <img id="imgPreview" src="/snapshots/last.jpg" />
+    </div>
+
+    <div name="meteo" id="meteo">
+        <div class="temperatures">
+            <iframe  frameborder="0"
+                scrolling="no" marginheight="0" marginwidth="0"
+                src="http://emoncms.jodaille.org/vis/multigraph?embed=1&mid=5">
+            </iframe>
+        </div>
+        <div class="luminosity">
+            <iframe frameborder="0"
+                scrolling="no" marginheight="0" marginwidth="0"
+                src="http://emoncms.jodaille.org/vis/multigraph?embed=1&mid=7">
+            </iframe>
+        </div>
+        <div class="humidity">
+            <iframe frameborder="0"
+                scrolling="no" marginheight="0" marginwidth="0"
+                src="http://emoncms.jodaille.org/vis/multigraph?embed=1&mid=9">
+            </iframe>
+        </div>
+        <div class="atmospheric">
+            <iframe frameborder="0"
+                scrolling="no" marginheight="0" marginwidth="0"
+                src="http://emoncms.jodaille.org/vis/multigraph?embed=1&mid=4">
+            </iframe>
+        </div>
+        <div class="rainfall">
+            <iframe frameborder="0"
+                scrolling="no" marginheight="0" marginwidth="0"
+                src="http://emoncms.jodaille.org/vis/multigraph?embed=1&mid=6">
+            </iframe>
+        </div>
+        <div class="rainlevel">
+            <iframe frameborder="0"
+                scrolling="no" marginheight="0" marginwidth="0"
+                src="http://emoncms.jodaille.org/vis/multigraph?embed=1&mid=8">
+            </iframe>
+        </div>
+    </div>
+
+    <div name="gitcode" class="gitcode">
+        <p>see code: https://github.com/Jodaille/LyceeDesAndaines/blob/master/snapshots/preview.php</p>
     </div>
 </body>
 
@@ -91,7 +222,8 @@ window.onload = function() {
     var spanTime              = document.getElementById("time");
     var spanSelectedImage     = document.getElementById("selectedImage");
     var currentImgLink        = document.getElementById("currentImgLink");
-
+    var displayMeteoLink      = document.getElementById("displayMeteo");
+    var meteo                 = document.getElementById("meteo");
 
       var keys = Object.keys(imagesDatas),
           len = keys.length,
@@ -108,7 +240,7 @@ window.onload = function() {
         {
             imagePreview.src     = value.src;
             spanTime.textContent = value.time;
-            currentImgLink.innerHTML = value.time;
+            currentImgLink.innerHTML = value.src;
             currentImgLink.setAttribute('href', imagePreview.src);
 
             imageID++;
@@ -117,10 +249,11 @@ window.onload = function() {
     }
     setInterval(changeImage, 200);
 
-    /*imagePreview.onclick = function() {
+    displayMeteoLink.onclick = function() {
 
-        console.log('src:' + imagePreview.src);
-    };*/
+        meteo.style.display = "inline";
+        //console.log('src:' + imagePreview.src);
+    };
 }
 
 
