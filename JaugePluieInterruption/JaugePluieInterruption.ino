526ec79c15d53c99eb38bfc9389f2b65264b7aa4
Jodaille <jody.noury@gmail.com>
2016-11-08 23:33:01 +0100

    add french comments

--- JaugePluieInterruption/JaugePluieInterruption.ino
+++ JaugePluieInterruption/JaugePluieInterruption.ino
@@ -1,39 +1,69 @@
 /*
-http://cactus.io/projects/weather/arduino-weather-station-rain-sensor
+ * Compteur hauteur d'eau tombée
+ * Source du script
+  http://cactus.io/projects/weather/arduino-weather-station-rain-sensor
+
+  le câblage consiste à relier un fil sur la masse (GND) l'autre
+  sur une pin ayant une fonction d'interruption (d2 ou d3) ici RG11_Pin
+  cf: https://www.arduino.cc/en/Reference/AttachInterrupt
 */
 
-// http://atelier-meteo-arduino-lycee-vincendo.blogspot.fr/p/le-pluviometre.html
-#define Bucket_Size 0.2794 // bucket size to trigger tip (count was 0.01)
+/*
+ volume du réservoir
+ d'après http://atelier-meteo-arduino-lycee-vincendo.blogspot.fr/p/le-pluviometre.html
+ il est de 0.2794mm
+ bucket size to trigger tip (count was 0.01)
+*/
+#define Bucket_Size 0.2794
 #define RG11_Pin 3 // digital pin RG11 connected to
 
+/*
+ bucket tip counter used in interrupt routine
+ comteur de bascule du reservoir
+*/
+volatile unsigned long tipCount;
 
-volatile unsigned long tipCount; // bucket tip counter used in interrupt routine
-volatile unsigned long contactTime; // Timer to manage any contact bounce in interrupt routine
-volatile float totalRainfall; // total amount of rainfall detected
+/*
+ minuteur de gestion du rebond de l'interrupteur
+ Timer to manage any contact bounce in interrupt routine
+*/
+volatile unsigned long contactTime;
+
+/*
+ hauteur totale détectée en mm/m2
+ total amount of rainfall detected
+*/
+volatile float totalRainfall;
 
 
 void setup() {
     Serial.begin(9600);
-    tipCount = 0;
-    totalRainfall = 0;
+    // initialisation des variables
+    tipCount = 0;      // nombre de basule(s)
+    totalRainfall = 0; // hauteur d'eau totale mm/m2
 
-    pinMode(RG11_Pin, INPUT_PULLUP);
+    // utilisation de la resistance du pin RG11_Pin
+    pinMode(RG11_Pin, INPUT_PULLUP); 
+
+    // attache sur la perte du signal du bouton
     attachInterrupt(digitalPinToInterrupt(RG11_Pin), isr_rg, FALLING);
-    sei();// Enable Interrupts
+    sei();// Enable Interrupts / activation de l'interruption
 }
 
 void loop() {
 
     Serial.print(totalRainfall); Serial.println(" mm");
 
-    // Add a 2 second delay.
-    delay(2000); //just here to slow down the output.
+    // Add a 2 second delay / delis de 2s
+    delay(2000); //just here to slow down the output. / ralentir l'affichage
 
 }
 
 // Interrupt handler routine that is triggered when the rg-11 detects rain
+// prise en charge de l'interruption déclanchée par la bacule
+// la boucle est interrompue par la bacule
 void isr_rg() {
-    if((millis() - contactTime) > 15 ) { // debounce of sensor signal
+    if((millis() - contactTime) > 15 ) { // debounce of sensor signal / anti-rebond
         tipCount++;
         totalRainfall = tipCount * Bucket_Size;
         contactTime = millis();
